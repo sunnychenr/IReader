3ae193dabb5d126595f42c1a34d22a6a3f48a429
chenrong <https://github.com/sunnychenr/GoogleLocation.git>
2018-04-19 15:57:53 +0800

    UI主界面框架

--- app/src/main/java/com/example/ireader/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/ireader/ui/activity/MainActivity.java
@@ -1,23 +1,31 @@
 package com.example.ireader.ui.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
-import android.view.View;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import com.example.ireader.R;
 import com.example.ireader.ui.BaseActivity;
+import com.example.ireader.util.LogUtil;
 import com.example.ireader.util.ToastUtil;
 
 /**
  * Created by ChenR on 2017/9/28.
  */
 
-public class MainActivity extends BaseActivity implements DrawerLayout.DrawerListener{
-    private DrawerLayout mLayoutRoot;
-    private View mLayoutContent;
-    private View mLayoutMenu;
-
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
     private long mLastBackTime = 0;
+
+    private Toolbar mToolBar = null;
+    private DrawerLayout mDrawerLayout = null;
+    private FrameLayout mLayoutContent = null;
+    private NavigationView mLayoutMenu = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +34,26 @@ public class MainActivity extends BaseActivity implements DrawerLayout.DrawerLis
 
     @Override
     protected void bindAction() {
-        mLayoutRoot.addDrawerListener(this);
+        // 设置随打开关闭动作变化的toolbar左侧按钮;
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                mDrawerLayout, mToolBar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        mLayoutMenu.setNavigationItemSelectedListener(this);
     }
 
     @Override
     protected void initData() {
-
     }
 
     @Override
     protected void initView() {
-        mLayoutRoot = findViewById(DrawerLayout.class, R.id.layout_root);
-        mLayoutContent = findViewById(R.id.layout_content);
-        mLayoutMenu = findViewById(R.id.layout_menu);
+        mDrawerLayout = findViewById(DrawerLayout.class, R.id.layout_root);
+        mToolBar = findViewById(Toolbar.class, R.id.tool_bar);
+        mLayoutContent = findViewById(FrameLayout.class, R.id.layout_content);
+        mLayoutMenu = findViewById(NavigationView.class, R.id.layout_menu);
     }
 
     @Override
@@ -47,35 +62,34 @@ public class MainActivity extends BaseActivity implements DrawerLayout.DrawerLis
     }
 
     @Override
-    public void onDrawerSlide(View drawerView, float slideOffset) {
-
-    }
-
-    @Override
-    public void onDrawerOpened(View drawerView) {
-
-    }
-
-    @Override
-    public void onDrawerClosed(View drawerView) {
-
-    }
-
-    @Override
-    public void onDrawerStateChanged(int newState) {
-
-    }
-
-    @Override
     public void onBackPressed() {
-        long now = System.currentTimeMillis();
-
-        if (now - mLastBackTime < 2000) {
-            super.onBackPressed();
+        if (mDrawerLayout.isDrawerOpen(Gravity.START)) {
+            mDrawerLayout.closeDrawer(Gravity.START);
         } else {
-            mLastBackTime = now;
-            ToastUtil.showToast(this, getString(R.string.exit_app_hint));
-        }
+            long now = System.currentTimeMillis();
 
+            if (now - mLastBackTime < 2000) {
+                super.onBackPressed();
+            } else {
+                mLastBackTime = now;
+                ToastUtil.showToast(this, getString(R.string.exit_app_hint));
+            }
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_read:
+                break;
+            case R.id.menu_item_file:
+                LogUtil.d("chenr", "menu item file click.");
+                break;
+            case R.id.menu_item_share:
+                break;
+            case R.id.menu_item_feedback:
+                break;
+        }
+        return false;
     }
 }
